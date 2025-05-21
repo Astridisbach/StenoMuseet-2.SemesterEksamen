a82c1d3b10da34bc004ba41e511ed59f4def274b
Astridisbach <astridisbachsorensen@gmail.com>
2025-05-21 13:25:17 +0200

    aller sidste ændring

---
+++ js/videoslut.js
@@ -0,0 +1,5 @@
+"use strict"; // Aktiverer strict mode – hjælper med at fange fejl og forbyder brug af visse usikre funktioner
+
+setTimeout(function() {
+    window.location.href = "index.html";
+  }, 124000);
