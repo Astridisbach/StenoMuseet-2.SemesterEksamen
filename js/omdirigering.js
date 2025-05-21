22c64541261138730c3030adc4da6095288faab0
Astridisbach <astridisbachsorensen@gmail.com>
2025-05-21 10:05:23 +0200

    stavefejl

--- js/omdirigering.js
+++ js/omdirigering.js
@@ -2,4 +2,7 @@
 
 setTimeout(function() {
     window.location.href = "index.html";
-  }, 7000); // 7000 ms = 7 sekunder og den omdirigere til index.html
+  }, 7000); // 7000 ms = 7 sekunder og den omdirigerer til index.html
+
+
+  
